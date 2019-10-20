034b6c8c73525d8e8ba8191827c80feb8166c9d2
buenj1 <47642049+buenj1@users.noreply.github.com>
2019-10-19 19:07:59 -0700

    commit of lab8

---
+++ TIVAC/Lab 8/src_code/Debug/task2.d
@@ -0,0 +1,47 @@
+# FIXED
+
+task2.obj: ../task2.c
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h
+task2.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h
+task2.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h
+
+../task2.c: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdbool.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h: 
