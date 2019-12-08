b4219de98fc2982ad3c8bf9154433dcbe91eccaf
buenj1 <47642049+buenj1@users.noreply.github.com>
2019-12-08 13:12:21 -0800

    commit of CC1350-Lab5

---
+++ CC1350-LABS/Lab5/src_code/sensor_CC1350_LAUNCHXL_tirtos_ccs/Release/portable/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../portable/main_tirtos.c \
+../portable/temperature.c 
+
+C_DEPS += \
+./portable/main_tirtos.d \
+./portable/temperature.d 
+
+OBJS += \
+./portable/main_tirtos.obj \
+./portable/temperature.obj 
+
+OBJS__QUOTED += \
+"portable\main_tirtos.obj" \
+"portable\temperature.obj" 
+
+C_DEPS__QUOTED += \
+"portable\main_tirtos.d" \
+"portable\temperature.d" 
+
+C_SRCS__QUOTED += \
+"../portable/main_tirtos.c" \
+"../portable/temperature.c" 
+
+
